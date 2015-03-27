ba99085b3835b5f51bd8a78fbdbb67f5d60655c0
lukeclarke56 <lukeclarke56@hotmail.co.uk>
2015-03-27 15:49:18 +0000

    contact.php
    Draft one of the contact form

---
+++ contact.php
@@ -0,0 +1,29 @@
+
+<body>
+
+	<div id="page-wrap">				
+		<div id="contact-area" align="center">
+			
+			<form method="post" action="contactengine.php">
+				<label for="Name">Name:</label>
+				<input type="text" name="Name" id="Name" placeholder="Your Name"/>
+	
+				<label for="Email">Email:</label>
+				<input type="text" name="Email" id="Email" placeholder="Email@domain.com" />
+				
+				<label for="Message">Message:</label><br />
+				<textarea name="Message" rows="20" cols="20" id="Message" placeholder="Write something to us" ></textarea>
+
+				<input type="submit" name="submit" value="Submit" class="submit-button" />
+			</form>
+			
+			<div style="clear: both;"></div>
+			
+			
+		</div>
+	
+	</div>
+
+</body>
+
+</html>

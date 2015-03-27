e9e7fccf264610ffad6e7a855aea9690e9c2095c
NibiaWaheed <Nibiawaheed@gmail.com>
2015-03-27 15:34:28 +0000

    login_header.php

---
+++ login_header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Login</title>
+        <link href="css/login_normal.css" rel="stylesheet">
+        <link href="css/login_style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="login-card h1">
+            <h1>Login</h1>    
+        </div>
+        
+    <div class="ouputmessage">
+    <?php echo message(); ?>
+        </div>

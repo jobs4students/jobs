9a8bbca32125679e514d48fc4be354b83aef43cf
lukeclarke56 <lukeclarke56@hotmail.co.uk>
2015-04-14 16:52:47 +0100

    Update home.php

--- home.php
+++ home.php
@@ -29,6 +29,51 @@
         </script>  
 </div>
 
+<div id="map-title" >
+<h1>Jobs Available in Bournemouth</h1>
+</div>
+
+<div id="map-canvas">
+           
+     <script type="text/javascript">
+        var locations = [
+          ['Sainsburys', 50.744903, -1.878044, 1],
+          ['lidl', 50.741434, -1.879711, 2],
+          ['Pizza Hut', 50.739985, -1.879077, 3],
+          ['Scissors', 50.739662, -1.8799, 4],
+          ['Waitrose', 50.740864, -1.87912, 5],
+          ['Boots', 50.741124, -1.879534, 6],
+          ['The Larkrise', 50.744481, -1.878202, 7],
+         
+        ];
+
+        var map = new google.maps.Map(document.getElementById('map-canvas'), {
+          zoom: 15,
+          center: new google.maps.LatLng(50.739888, -1.879381),
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+
+        var infowindow = new google.maps.InfoWindow();
+
+        var marker, i;
+
+        for (i = 0; i < locations.length; i++) {
+          marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+          });
+         
+
+          google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+              infowindow.setContent(locations[i][0]);
+              infowindow.open(map, marker);
+            }
+          })(marker, i));
+        }
+      </script>
+    
+    </div>
 
 
         

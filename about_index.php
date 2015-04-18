7a01915e802ad8a918d3b235992bc80489b9f45c
Mark Lucas <markjl1994@hotmail.com>
2015-04-18 21:36:49 +0100

    Create about_index.php

---
+++ about_index.php
@@ -0,0 +1,79 @@
+<?php include_once("../includes/templates/aboutheader.php"); ?>
+<?php include_once("../public/about_nav.php"); ?>
+
+
+
+        <div id="header">
+            
+        </div>
+
+           <div id="about" align="center">
+            <div class="containermain" >
+                <h1>About Us</h1>
+            </div>
+          <div class="grid">      
+        <ul class="grid cs-style-4">
+            <li>
+                <figure>
+				    <div><img src="images/nibia1.jpg" alt="img05" width="400" height="300"></div>
+				<figcaption>
+				    <h3>Nibia Waheed</h3>
+                    <p>Hi, my name is Nibia Waheed. I am 19 years old and Bournemouth University student stuying Digital Media Design.</p>
+				    <a href="https://nibiawaheed.wordpress.com/" target="_blank">Checkout my blog!</a>
+				</figcaption>
+				</figure>
+            </li>
+            <li>
+				<figure>
+				    <div><img src="images/mark1.jpg" alt="img06" width="400" height="300"></div>
+				<figcaption>
+				    <h3>Mark Lucas</h3>
+                    <p>Hello! My name is Mark Lucas. I am 20 years old and I am studying Digital Media Design at Bournemouth university.</p>
+				    <a href="https://markjlucas.wordpress.com/" target="_blank">Checkout my blog!</a>
+				</figcaption>
+				</figure>
+            </li>
+            <li>
+                <figure>
+				    <div><img src="images/Luke1.jpg" alt="img02" width="400" height="300"></div>
+				<figcaption>
+				    <h3>Luke Clarke</h3>
+                    <p>Hello! My name is Luke Clarke. I am 19 years old and I am studying Digital Media Design at Bournemouth university.</p>
+				    <a href="https://lukeclarke56.wordpress.com/" target="_blank">Checkout my blog!</a>
+				</figcaption>
+				</figure>
+            </li>
+            <li>
+                <figure>
+				    <div><img src="images/grace1.jpg" alt="img02" width="400" height="300"></div>
+				<figcaption>
+				    <h3>Grace Miller</h3>
+                    <p>Hello! My name is Grace Miller. I am 18 years old and I am studying Digital Media Design at Bournemouth university.</p>
+				    <a href="https://gracemiller07.wordpress.com/" target="_blank">Checkout my blog!</a>
+				</figcaption>
+				</figure>
+            </li>
+            <li>
+                <figure>
+				    <div><img src="images/Ashley1.jpg" alt="img02" width="400" height="300"></div>
+				<figcaption>
+				    <h3>Ashley Howatson-Tout</h3>
+                    <p>Hello! My name is Ashley Howatson-Tout. I am 20 years old and I am studying Digital Media Design at Bournemouth university.</p>
+				    <a href="http://ashleylht.tumblr.com" target="_blank">Checkout my blog!</a>
+				</figcaption>
+				</figure>
+            </li>
+            <li>
+                <figure>
+				    <div><img src="images/nic.jpg" alt="img02" width="400" height="300"></div>
+				<figcaption>
+				    <h3>Nic Baxter</h3>
+                    <p>Hello....</p>
+				    <a href="#" target="_blank">Checkout my blog!</a>
+				</figcaption>
+				</figure>
+            </li>
+			</ul>
+		</div>
+               
+<?php include_once("../includes/templates/footer.php"); ?>

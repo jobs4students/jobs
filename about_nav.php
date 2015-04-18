c042159ae2922a1139fdb46c91dd878dd6b96acd
Mark Lucas <markjl1994@hotmail.com>
2015-04-18 21:37:19 +0100

    Create about_nav.php

---
+++ about_nav.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" type="text/css" href="css/nav.css">
+
+<div id="header">
+            <div id='cssmenu' align="center">
+                <ul>
+                    <li><a href='../public/home_index.php'><span>Home</span></a></li>
+                    <li><a href='../public/job_index.php'><span>Jobs</span></a></li>
+                    <li class='active'><a href='../public/about_index.php'><span>About</span></a></li>
+                    <li><a href='../public/contact_index.php'><span>Contact</span></a></li>
+                </ul>
+            </div>
+        </div>

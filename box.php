6192603e4716e4b8766251f546c399411ac5b9ee
lukeclarke56 <lukeclarke56@hotmail.co.uk>
2015-04-20 09:24:03 +0100

    box.php

---
+++ box.php
@@ -0,0 +1,23 @@
+<div class="box">
+<table>   
+   <tr>
+    <th><?php echo $row["username"]; ?></th>
+    </tr>
+    <tr>
+    <td><?php echo $row["company"]; ?></td>
+    </tr> 
+    <tr>
+    <td><?php echo $row["job_position"]; ?></td>
+    </tr>
+    <tr>
+    <td><?php echo $row["part_full_time"]; ?></td>
+    </tr> 
+    <tr>
+    <td><?php echo $row["location"]; ?></td>
+    </tr>
+    <tr>
+    <td><?php echo $row["job_description"]; ?></td>
+    </tr>
+</table>       
+</div>
+
